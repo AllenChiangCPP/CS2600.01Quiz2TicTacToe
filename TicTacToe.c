64645d168a277d2be4aaa7f4022f5090372148db
Allen <allenchiang@cpp.edu>
2022-10-05 19:39:39 -0700

    added more comments to plan out code and added functions for later use

---
+++ TicTacToe.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+//Tic Tac Toe game between 2 players or player vs computer
+
+int playerVsPlayer() //player vs player game, if 
+{
+    //encase in while loop for game duration, maybe use a 2d array to track the game?
+    return 0;
+}
+int playerVsComputer()
+{
+    return 0;
+}
+int winChecker(int givenBoard[8]) //function that checks if there is a winner based on inputted array
+{
+    return 0;
+}
+
+int main()
+{
+    //create an empty board, an int array with a size of 8 to represent the grid
+        //  0,1,2
+        //  3,4,5
+        //  6,7,8
+
+    //prompt user for game they wish to play, encase in loop, break if user inputs a valid unput
+    
+    //read information from console, 1 = player vs player, 2 = player vs computer, anything else = invalid input
+
+        //if 1 is chosen, break out of loop, starts player v player game as playerVsPlayer() function
+        //if 2 is chosen, break out of loop, start player vs computer game as playerVsComputer() function
+        //else, print invalid input and ask for input again
+
+    
+    return 0;
+
+}
