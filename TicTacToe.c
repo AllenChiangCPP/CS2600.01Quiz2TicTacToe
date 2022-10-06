bd9c6e35ca14a6170c7c4de4a3cf3ba94f03bc4d
Allen <allenchiang@cpp.edu>
2022-10-05 23:28:12 -0700

    added comments and plan on coding the play again feature

--- TicTacToe.c
+++ TicTacToe.c
@@ -17,39 +17,52 @@ int winChecker(char givenBoard[8]) //function that checks if there is a winner b
 
 int main()
 {
-    int userGamePick;
-    //int pickGameStatus = 1; //int for while loop, if while loop is 1
-    //create an empty board, a character array with a size of 8 to represent the grid
+    while (1)
+    {
+        int userGamePick;
+        //int pickGameStatus = 1; //int for while loop, if while loop is 1
+        //create an empty board, a character array with a size of 8 to represent the grid
         //  0,1,2
         //  3,4,5
         //  6,7,8
-    printf("WELCOME TO TIC TAC TOE!\n");
-    while (1)
-    {
-        int userGameInput;
-        printf("1: Person vs. Person\n");
-        printf("2: Person vs. Computer\n");
-        printf("Enter your choice (1 or 2): \n");
-        scanf("%d", &userGameInput);
-        if (userGameInput == 1 || userGameInput == 2)
-        {
-            userGamePick = userGameInput;
-            break;
-        }
-        printf("INVALID RESPONSE\n");
-        getchar();
-            //pickGameStatus = 1;
-    }
-    printf("You have entered choice %d\n", userGamePick);
-    
-    
-    //prompt user for game they wish to play, encase in loop, break if user inputs a valid unput
-    
-    //read information from console, 1 = player vs player, 2 = player vs computer, anything else = invalid input
-
+        printf("WELCOME TO TIC TAC TOE!\n");
         //if 1 is chosen, break out of loop, starts player v player game as playerVsPlayer() function
         //if 2 is chosen, break out of loop, start player vs computer game as playerVsComputer() function
         //else, print invalid input and ask for input again
+        //prompt user for game they wish to play, encase in loop, break if user inputs a valid unput
+        //read information from console, 1 = player vs player, 2 = player vs computer, anything else = invalid input
+        while (1)
+        {
+            int userGameInput;
+            printf("1: Person vs. Person\n");
+            printf("2: Person vs. Computer\n");
+            printf("Enter your choice (1 or 2): \n");
+            scanf("%d", &userGameInput);
+            if (userGameInput == 1 || userGameInput == 2)
+            {
+                userGamePick = userGameInput;
+                break;
+            }
+            printf("INVALID RESPONSE\n");
+            getchar();
+            //pickGameStatus = 1;
+        }
+        printf("You have entered choice %d\n", userGamePick);
+        if (userGamePick == 1)
+        {
+            playerVsPlayer();
+        }
+        if (userGamePick == 2)
+        {
+            playerVsComputer();
+        }
+        //ask the user if they want to play again, 1 == yes, 2 == no
+        //if user inputs 1, loop continues, if user inputs 2, exit loop, if neither 1 or 2, print invalid input 
+    }
+    
+   
+
+
 
     
     return 0;
