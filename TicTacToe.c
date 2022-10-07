a986616704c06c95dc944255ba3539c663a05c14
Allen <allenchiang@cpp.edu>
2022-10-06 23:53:35 -0700

    Coverted code to use 2d arrays instead of 1d arrays.

--- TicTacToe.c
+++ TicTacToe.c
@@ -5,7 +5,7 @@
 
 //modify win checker by changing it to take in and check a 2d array 
     //givenBoard[1] -> givenBoard[0][0], givenBoard[2] -> givenBoard[0][1], etc
-int winChecker(char givenBoard[9]) //function that checks if there is a winner based on inputted array
+int winChecker(char givenBoard[3][3]) //function that checks if there is a winner based on inputted array
 {
     //0 1 2
     //3 4 5
@@ -18,99 +18,99 @@ int winChecker(char givenBoard[9]) //function that checks if there is a winner b
         //if given int array is full and no winner, declare a draw
         //return 1 for player 1 win, 2 for player 2 win, 3 for draw, 0 by default
     //ROW CHECKS
-    if(givenBoard[0] == givenBoard[1] && givenBoard[1] == givenBoard[2]) 
+    if(givenBoard[0][0] == givenBoard[0][1] && givenBoard[0][1] == givenBoard[0][2]) 
     {
-        if(givenBoard[0] == 'X')
+        if(givenBoard[0][0] == 'X')
         {
             return 1;
         }
-        if(givenBoard[0] == 'O')
+        else if(givenBoard[0][0] == 'O')
         {
             return 2;
         }
     }
-    else if(givenBoard[3] == givenBoard[4] && givenBoard[4] == givenBoard[5]) 
+    else if(givenBoard[1][0] == givenBoard[1][1] && givenBoard[1][1] == givenBoard[1][2])
     {
-        if(givenBoard[3] == 'X')
+        if(givenBoard[1][0] == 'X')
         {
             return 1;
         }
-        if(givenBoard[3] == 'O')
+        else if(givenBoard[1][0] == 'O')
         {
             return 2;
         }
     }
-    else if(givenBoard[6] == givenBoard[7] && givenBoard[7] == givenBoard[8]) 
+    else if(givenBoard[2][0] == givenBoard[2][1] && givenBoard[2][1] == givenBoard[2][2])
     {
-        if(givenBoard[6] == 'X')
+        if(givenBoard[2][0] == 'X')
         {
             return 1;
         }
-        if(givenBoard[6] == 'O')
+        else if(givenBoard[2][0] == 'O')
         {
             return 2;
         }
     }
     //COLUMN CHECKS
-    else if(givenBoard[0] == givenBoard[3] && givenBoard[3] == givenBoard[6]) 
+    else if(givenBoard[0][0] == givenBoard[1][0] && givenBoard[1][0] == givenBoard[2][0])
     {
-        if(givenBoard[0] == 'X')
+        if(givenBoard[0][0] == 'X')
         {
             return 1;
         }
-        if(givenBoard[0] == 'O')
+        else if(givenBoard[0][0] == 'O')
         {
             return 2;
         }
     }
-    else if(givenBoard[1] == givenBoard[4] && givenBoard[4] == givenBoard[7]) 
+    else if(givenBoard[0][1] == givenBoard[1][1] && givenBoard[1][1] == givenBoard[2][1]) 
     {
-        if(givenBoard[1] == 'X')
+        if(givenBoard[0][1] == 'X')
         {
             return 1;
         }
-        if(givenBoard[1] == 'O')
+        else if(givenBoard[0][1] == 'O')
         {
             return 2;
         }
     }
-    else if(givenBoard[2] == givenBoard[5] && givenBoard[5] == givenBoard[8]) 
+    else if(givenBoard[0][2] == givenBoard[1][2] && givenBoard[1][2] == givenBoard[2][2])
     {
-        if(givenBoard[2] == 'X')
+        if(givenBoard[0][2] == 'X')
         {
             return 1;
         }
-        if(givenBoard[2] == 'O')
+        else if(givenBoard[0][2] == 'O')
         {
             return 2;
         }
     }
     //DIAGONAL
-    else if(givenBoard[0] == givenBoard[4] && givenBoard[4] == givenBoard[8]) 
+    else if(givenBoard[0][0] == givenBoard[1][1] && givenBoard[1][1] == givenBoard[2][2])
     {
-        if(givenBoard[0] == 'X')
+        if(givenBoard[0][0] == 'X')
         {
             return 1;
         }
-        if(givenBoard[0] == 'O')
+        else if(givenBoard[0][0] == 'O')
         {
             return 2;
         }
     }
-    else if(givenBoard[2] == givenBoard[4] && givenBoard[4] == givenBoard[6]) 
+    else if(givenBoard[2][0] == givenBoard[1][1] && givenBoard[1][1] == givenBoard[0][2])
     {
-        if(givenBoard[2] == 'X')
+        if(givenBoard[2][0] == 'X')
         {
             return 1;
         }
-        if(givenBoard[2] == 'O')
+        else if(givenBoard[2][0] == 'O')
         {
             return 2;
         }
     }
     //FULL BOARD
-    else if(givenBoard[0] != '1'&& givenBoard[1] != '2'&& givenBoard[2] != '3'&& givenBoard[3] != '4' &&
-    givenBoard[4] != '5'&& givenBoard[5] != '6'&& givenBoard[6] != '7' && givenBoard[7] != '8' && givenBoard[8] != '9')
+    else if(givenBoard[0][0] != ' ' && givenBoard[0][1] != ' ' && givenBoard[0][2] != ' ' && givenBoard[1][0] != ' ' 
+    && givenBoard[1][1] != ' ' && givenBoard[1][2] != ' ' && givenBoard[2][0] != ' ' && givenBoard[2][1] != ' ' && givenBoard[2][2] != ' ')
     {
         return 3;
     }
@@ -119,23 +119,24 @@ int winChecker(char givenBoard[9]) //function that checks if there is a winner b
 
 //modify board printer to take in a 2d array instead of a regular array
     //givenBoard[1] -> givenBoard[0][0], givenBoard[2] -> givenBoard[0][1], etc
-void boardPrinter(char givenBoard[9]) //fuction that takes a char array and prints the board
+void boardPrinter(char givenBoard[3][3]) //fuction that takes a char array and prints the board
 {
-    printf("+-----------+\n");
-    printf("| %c | %c | %c | \n",givenBoard[0], givenBoard[1], givenBoard[2]);
-    printf("+-----------+\n");
-    printf("| %c | %c | %c | \n",givenBoard[3], givenBoard[4], givenBoard[5]);
-    printf("+-----------+\n");
-    printf("| %c | %c | %c | \n",givenBoard[6], givenBoard[7], givenBoard[8]);
-    printf("+-----------+\n");
+    printf("   | 0:| 1:| 2:|\n");
+    printf("   +---+---+---+\n");
+    printf("0: | %c | %c | %c | \n", givenBoard[0][0], givenBoard[0][1], givenBoard[0][2]);
+    printf("   +-----------+\n");
+    printf("1: | %c | %c | %c | \n", givenBoard[1][0], givenBoard[1][1], givenBoard[1][2]);
+    printf("   +-----------+\n");
+    printf("2: | %c | %c | %c | \n", givenBoard[2][0], givenBoard[2][1], givenBoard[2][2]);
+    printf("   +-----------+\n");
 }
 
 //modify to work with a 2d array, take 2 user inputs from same scanf line to represent row and column
 int playerVsPlayer() //player vs player game, prints who is the winner 
 {
-    char gameBoard[9] = {'1','2','3','4','5','6','7','8','9'}; 
+    //char gameBoard[9] = {'1','2','3','4','5','6','7','8','9'}; 
     //make a blank 2d array to represent the gameboard
-    //char gameBoard[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}; 
+    char gameBoard[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}}; 
     while(1)
     {
         //create an empty board, an char array with a size of 8 to represent the grid, player 1 is represented as X while player 2 is represented as O
@@ -153,15 +154,18 @@ int playerVsPlayer() //player vs player game, prints who is the winner
         boardPrinter(gameBoard);
         while (1)
         {
-            int player1Input = 10;
-            printf("Player 1 Turn: \n");
-            scanf("%d", &player1Input);
-            if (gameBoard[player1Input-1] != 'X' && gameBoard[player1Input-1] != 'O' && player1Input > 0 && player1Input < 10)
+            int player1RowInput = 3;
+            int player1ColumnInput = 3;
+            printf("Player 1 Turn. (row column): \n");
+            scanf("%d %d", &player1RowInput, &player1ColumnInput);
+            if (gameBoard[player1RowInput][player1ColumnInput] == ' ' && player1RowInput >= 0 && player1RowInput < 3 
+            && player1ColumnInput >= 0 && player1ColumnInput < 3)
             {
-                gameBoard[player1Input-1] = 'X';
+                gameBoard[player1RowInput][player1ColumnInput] = 'X';
                 break;
             }
             printf("INVALID RESPONSE\n");
+            getchar();
             getchar();
         }
         if(winChecker(gameBoard) == 1)
@@ -185,15 +189,18 @@ int playerVsPlayer() //player vs player game, prints who is the winner
         boardPrinter(gameBoard);
         while (1)
         {
-            int player2Input = 10;
-            printf("Player 2 Turn: \n");
-            scanf("%d", &player2Input);
-            if (gameBoard[player2Input-1] != 'X' && gameBoard[player2Input-1] != 'O' && player2Input > 0 && player2Input < 10)
+            int player2RowInput = 3;
+            int player2ColumnInput = 3;
+            printf("Player 2 Turn. (row column): \n");
+            scanf("%d %d", &player2RowInput, &player2ColumnInput);
+            if (gameBoard[player2RowInput][player2ColumnInput] == ' ' && player2RowInput >= 0 && player2RowInput < 3 
+            && player2ColumnInput >= 0 && player2ColumnInput < 3)
             {
-                gameBoard[player2Input-1] = 'O';
+                gameBoard[player2RowInput][player2ColumnInput] = 'O';
                 break;
             }
             printf("INVALID RESPONSE\n");
+            getchar();
             getchar();
         }
         if(winChecker(gameBoard) == 1)
@@ -221,27 +228,31 @@ int playerVsComputer() //player vs computer game, prints who is the winner, comp
 {
     //copy most of the code from player vs player, modify player 2 section 
     //encase in while loop for game duration, ends if there is a winner or draw
-    char gameBoard[9] = {'1','2','3','4','5','6','7','8','9'}; 
+    //char gameBoard[9] = {'1','2','3','4','5','6','7','8','9'}; 
+    char gameBoard[3][3] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
     while(1)
     {
         //prints game board and current players turn, game board has numbers to indicate which spaces available, will be filled out with an X for player 1 or and O for computer
-        //0|1|2
-        //3|4|5
-        //6|7|8
+        //0:|[0][0]|[0][0]|[0][0]|
+        //1:|[0][0]|[0][0]|[0][0]|
+        //2:|[0][0]|[0][0]|[0][0]|
 
     //asks player 1 for input and checks validity
         boardPrinter(gameBoard);
         while (1)
         {
-            int player1Input = 10;
-            printf("Player 1 Turn: \n");
-            scanf("%d", &player1Input);
-            if (gameBoard[player1Input-1] != 'X' && gameBoard[player1Input-1] != 'O' && player1Input > 0 && player1Input < 10)
+           int player1RowInput = 3;
+            int player1ColumnInput = 3;
+            printf("Player 1 Turn. (row column): \n");
+            scanf("%d %d", &player1RowInput, &player1ColumnInput);
+            if (gameBoard[player1RowInput][player1ColumnInput] == ' ' && player1RowInput >= 0 && player1RowInput < 3 
+            && player1ColumnInput >= 0 && player1ColumnInput < 3)
             {
-                gameBoard[player1Input-1] = 'X';
+                gameBoard[player1RowInput][player1ColumnInput] = 'X';
                 break;
             }
             printf("INVALID RESPONSE\n");
+            getchar();
             getchar();
         }
         //runs win checker to see if there is a winner or draw
@@ -268,15 +279,15 @@ int playerVsComputer() //player vs computer game, prints who is the winner, comp
         while (1)
         {
             srand(time(NULL));
-            int computerInput = rand() % 9; 
-            //scanf("%d", &player2Input);
-            if (gameBoard[computerInput] != 'X' && gameBoard[computerInput] != 'O' && computerInput >= 0 && computerInput < 9)
+            int computerRowInput = rand() % 3; 
+            int computerColumnInput = rand() % 3;
+            if (gameBoard[computerRowInput][computerColumnInput] == ' ' && computerRowInput >= 0 && computerRowInput < 3 
+            && computerColumnInput >= 0 && computerColumnInput < 3)
             {
-                printf("%d\n", computerInput+1);
-                gameBoard[computerInput] = 'O';
+                printf("%d %d\n", computerRowInput, computerColumnInput);
+                gameBoard[computerRowInput][computerColumnInput] = 'O';
                 break;
             }
-            //getchar();
         }
         if(winChecker(gameBoard) == 1)
         {
